8f4130ba6738dd5e33d2ecdbcdcca796dee6e738
binam33 <bi.nam@osckorea.com>
2022-11-16 13:54:53 +0900

    update for k8s test

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # simple-api
+
 이 프로젝트 springboot기반  simple-api project입니다.
 
 주로 단순한 서비스 연동, 클러스터에서의 서비스 확인,CICD테스트 등의 용도로 사용됩니다.
@@ -12,3 +13,18 @@ maven wrapper를 통해 다음과 같이 실행하시기 바랍니다.
 #run
 ./mvnw spring-boot:run
 ```
+
+## 클러스터에 빌드하고 배포하기
+
+skaffold를 이용하여 -p(profile), -t(version-git tag) 옵션을 적절히 주고 실행하면 해당 클러스터에 디버깅 모드로 배포됩니다.
+
+```bash
+# debug mode
+skaffold dev -p dev -t 0.0.1 
+# build
+skaffold build -p dev -t 0.0.1 
+# run
+skaffold run -p dev -t 0.0.1 
+```
+
+클러스터에 요청을 보내어 로그가 정상적으로 들어오는지 확인한다.
--- src/main/java/com/example/simpleapi/controller/SimpleApiController.java
+++ src/main/java/com/example/simpleapi/controller/SimpleApiController.java
@@ -1,13 +1,13 @@
 package com.example.simpleapi.controller;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import com.example.simpleapi.model.Simple;
-
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.simpleapi.model.Simple;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,30 +16,30 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SimpleApiController {
 	@GetMapping("/hello")
-	public String hello(@RequestParam String name) throws Exception {
-		
-		log.info("==========simple-api home({})",name);
-		return "hello world :"+name;
-		
+	public String hello() throws Exception {
+
+		log.info("==========simple-api hello world");
+		return "hello world!!!";
+
 	}
+
 	@GetMapping("/simple")
-	public List<Simple> listSimple(){
+	public List<Simple> listSimple() {
 		List<Simple> list = new ArrayList<>();
-		
-		for(int i=0 ; i< 10;i++) {
-			list.add(new Simple(i+1,"test-"+i, "contents-"+i));
-			log.info("for "+i);
+
+		for (int i = 0; i < 10; i++) {
+			list.add(new Simple(i + 1, "test-" + i, "contents-" + i));
+			log.info("for " + i);
 		}
 		log.info(list.toString());
 		return list;
-		
 	}
-	
+
 	@GetMapping("/version")
-	public String version(){
-		//log.info("version 1.0");
+	public String version() {
+		log.info("version 1.0");
 		return "=====  version 1.0";
-		
+
 	}
 
 }
